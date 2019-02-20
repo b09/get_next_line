a56f628b9a719f44ea99ce19390d4fd07beb5716
Brendan Prado <bprado@f1r5s11.codam.nl>
2019-02-20 23:33:04 +0100

    changes made

--- get_next_line.c
+++ get_next_line.c
@@ -12,9 +12,6 @@
 
 #include "get_next_line.h"
 #include "libft/libft.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
 
 // allowed libc functions: read, malloc, free
 
@@ -22,38 +19,58 @@
 // return value 1, 0, or -1 for line was read, reading completed  or error happened
 // must output result without \n
 
+static int		gnl_part2(char buff[], long long bytsRead, char **line, const int openfd, char *str)
+{
+	static char 	*statstr;
+
+	if (statstr != NULL)
+		str = ft_strjoin(str, statstr);
+	while (bytsRead > 0)
+	{
+		if (bytsRead < BUFF_SIZE)
+		{
+			buff[bytsRead] = 0;
+			*line = ft_strjoin(str, buff);
+			return (0);
+		}
+		if (ft_strchr(buff, '\n'))
+		{
+			statstr = ft_strdup((ft_strchr(buff, '\n') + 1));
+			buff[BUFF_SIZE - ft_strlen(ft_strchr(buff, '\n'))] = 0;
+			*line = ft_strjoin(str, buff);
+			return (1);
+		}
+		str = ft_strjoin(str, buff);
+		bytsRead = read(openfd, buff, BUFF_SIZE);
+		buff[BUFF_SIZE] = 0;
+	}
+	return (0);
+}
+
 int		get_next_line(const int openfd, char **line)
 {
-	char	buff[BUFF_SIZE + 1];
-	int		readRetInt;
-	char 	*str;
-	str = "";
-	int		j;
+	char			buff[BUFF_SIZE + 1];
+	long long		bytsRead;
+	int				i;
+	char 			*str;
 
+	str = ft_strnew(0);
 	if (!(line))
 		return (-1);
-	readRetInt = read(openfd, buff, BUFF_SIZE);
-	// j = readRetInt;
+	bytsRead = read(openfd, buff, BUFF_SIZE);
 	buff[BUFF_SIZE] = 0;
-	if (readRetInt == 0)
-	{
-		str = ft_strjoin(str, buff);
-		*line = str;
-		return (readRetInt);
-	}
-	if (readRetInt == -1)
-		return (readRetInt);
-	while (readRetInt > 0)// && !(ft_strchr(buff, '\0')))
-	{
-		buff[BUFF_SIZE] = 0;
-		if (readRetInt < BUFF_SIZE)//(ft_strchr(buff, '\0'))
-			buff[readRetInt] = 0;
-		str = ft_strjoin(str, buff);
-		readRetInt = read(openfd, buff, BUFF_SIZE);
-	}
-	*line = str;
-	return (1);
+	if (bytsRead < 0)
+		return (-1);
+	i = gnl_part2(buff, bytsRead, line, openfd, str);
+	return (i);
 }
+
+
+
+// line is str 
+// str is ft_strjoin(str, buff) 
+
+
 
 
 //strjoin will copy all elements of string array up until null char, and will add null to the end of the copied string
@@ -81,8 +98,15 @@ int		main(int argc, char *argv[])
 
 	if (openfd != -1){
 		get_next_line(openfd, &line);
+		printf("----This is string 1: %s\n", line);
+		get_next_line(openfd, &line);
+		printf("----This is string 2: %s\n", line);
+		get_next_line(openfd, &line);
+		printf("----This is string 3: %s\n", line);
+		get_next_line(openfd, &line);
+		printf("----This is string 4: %s\n", line);
 	}
-	printf("This is the first string: \n%s", line);// line[0]);
+	// printf("This is the first string: \n%s", line);// line[0]);
 	return (0);
 }
 
--- get_next_line.h
+++ get_next_line.h
@@ -13,6 +13,9 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 10
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int		get_next_line(const int fd, char **line);
 #endif
